dcbdecca53dfdee3c169461b9357f54f67d305d6
widyadana774 <ranggawidyadana@gmail.com>
2026-05-02 17:31:57 +0700

    Update TFT_eSPI.cpp

--- lib/TFT_eSPI/TFT_eSPI.cpp
+++ lib/TFT_eSPI/TFT_eSPI.cpp
@@ -82,7 +82,7 @@ inline void TFT_eSPI::begin_tft_write(void){
   if (locked) {
     locked = false; // Flag to show SPI access now unlocked
 #if defined (SPI_HAS_TRANSACTION) && defined (SUPPORT_TRANSACTIONS) && !defined(TFT_PARALLEL_8_BIT) && !defined(RP2040_PIO_INTERFACE)
-    spi.beginTransaction(SPISettings(SPI_FREQUENCY, MSBFIRST, TFT_SPI_MODE));
+    // spi.beginTransaction(SPISettings(SPI_FREQUENCY, MSBFIRST, TFT_SPI_MODE));
 #endif
     CS_L;
     SET_BUS_WRITE_MODE;  // Some processors (e.g. ESP32) allow recycling the tx buffer when rx is not used
