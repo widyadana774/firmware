ca3d29f128b30ec58e17bbb8546e099b3bd3e873
widyadana774 <ranggawidyadana@gmail.com>
2026-05-02 20:20:57 +0700

    Update TFT_eSPI.cpp

--- lib/TFT_eSPI/TFT_eSPI.cpp
+++ lib/TFT_eSPI/TFT_eSPI.cpp
@@ -84,7 +84,7 @@ inline void TFT_eSPI::begin_tft_write(void){
 #if defined (SPI_HAS_TRANSACTION) && defined (SUPPORT_TRANSACTIONS) && !defined(TFT_PARALLEL_8_BIT) && !defined(RP2040_PIO_INTERFACE)
     // spi.beginTransaction(SPISettings(SPI_FREQUENCY, MSBFIRST, TFT_SPI_MODE));
 #endif
-    CS_L;
+    digitalWrite(TFT_CS, LOW);
     SET_BUS_WRITE_MODE;  // Some processors (e.g. ESP32) allow recycling the tx buffer when rx is not used
   }
 }
