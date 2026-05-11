e4a1c8f591869cb096fe4b88ede55d33d12d39c9
widyadana774 <ranggawidyadana@gmail.com>
2026-05-11 08:38:30 +0700

    Create User_Setup_Select.h

---
+++ lib/TFT_eSPI/User_Setup_Select.h
@@ -0,0 +1,5 @@
+// KOSONGKAN FILE INI - Bruce akan pakai build flags dari platformio.ini
+#ifndef USER_SETUP_LOADED
+#define USER_SETUP_LOADED
+// Semua definisi pin dan driver diambil dari build flags -Dxxx
+#endif
